f3d49b20d9edfb817df06fd5bd0182ff71c8bdad
Bhagwat-45 <cbh.mavim@gmail.com>
2026-05-02 23:56:49 +0530

    Add API endpoint to retrieve a single post and set up CI workflow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -29,7 +28,17 @@ templates = Jinja2Templates(directory="templates")
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html",{"posts":posts,"title" : "Home"})
 
-
 @app.get("/api/posts")
 def get_posts():
     return posts
+
+@app.get("/api/posts/{post_id}")
+def get_post(post_id:int):
+    for post in posts: 
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = "Post not found")
+
+
+
+
