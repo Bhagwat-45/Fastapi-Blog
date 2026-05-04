3e1fea2335046c2a9e651ec503ef89ed04e9471d
Chinmay Bhagwat <cbh.mavim@gmail.com>
2026-05-04 22:09:10 +0530

    L5 (#6)
    * Refactor project structure and add SQLAlchemy support
    
    - Updated .gitignore to exclude database files.
    - Modified main.py to include SQLAlchemy and new media route.
    - Added pyproject.toml and uv.lock dependencies for SQLAlchemy.
    - Removed old schema.py and created new schemas for posts and users.
    - Implemented database connection and models for posts and users.
    - Added user schema for Pydantic validation.
    
    * Remove unused CSS, icons, and templates to streamline project structure and improve maintainability.

--- main.py
+++ main.py
@@ -1,111 +1,21 @@
 from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from schema import PostResponse, PostCreate
+from database.database import Base, engine
+from models.posts_model import Post
+from models.users_model import User
+from router import post_router, user_router
 
-posts: list[dict] = [
-    {
-        "id" : 1,
-        "author" : "Chinmay Bhagwat",
-        "title" : "FastAPI is awesome",
-        "content" : "This framework is really easy to use and super fast",
-        "date_posted" : "April 20, 2025",
-    },
-    {
-        "id" : 2,
-        "author" : "Chetan Bhagwat",
-        "title" : "Go is great for Backend",
-        "content" : "Go is fast and it has good documents",
-        "date_posted" : "April 28, 2025",
-    }
-]
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/",include_in_schema=False,name="home")
-@app.get("/posts",include_in_schema=False,name="posts")
-def home(request: Request):
-    return templates.TemplateResponse(request, "home.html",{"posts":posts,"title" : "Home"})
-
-@app.get("/posts/{post_id}",include_in_schema=False)
-def post_page(request: Request,post_id: int):
-    for post in posts:
-        if post['id'] == post_id:
-            title = post['title'][:50]
-            return templates.TemplateResponse(request, "post.html", {"post": post, "title": title})
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Post not Found")
+app.include_router(post_router.router)
+app.include_router(user_router.router)
 
 
-@app.get("/api/posts",response_model=list[PostResponse])
-def get_posts():
-    return posts
 
-@app.get("/api/posts/{post_id}",response_model=PostResponse)
-def get_post(post_id:int):
-    for post in posts: 
-        if post.get("id") == post_id:
-            return post
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = "Post not found")
+@app.get("/", status_code=status.HTTP_200_OK)
+def home():
+    return {"message": "Hello"}
 
-@app.post("/api/posts",response_model=PostResponse,status_code=status.HTTP_201_CREATED)
-def create_post(post: PostCreate):
-    new_id = max(p["id"] for p in posts) + 1 if posts else 1
-    new_post = {
-        "id" : new_id,
-        "author": post.author,
-        "title": post.title,
-        "content": post.content,
-        "date_posted": "April 23, 2026"
-        }
-    
-    posts.append(new_post)
-    return new_post
-
-
-@app.exception_handler(StarletteHTTPException)
-def general_http_exception_handler(request: Request, exception: HTTPException):
-    message = (
-        exception.detail
-        if exception.detail
-        else "An error occured. Please check your request and try again.")
-
-    if request.url.path.startswith("/api"):
-        return JSONResponse(
-            exception= exception.status_code,
-            content={"detail": message}
-        )
-    
-    return templates.TemplateResponse(
-        request,
-        "error.html",
-        {
-            "status_code": exception.status_code,
-            "title":exception.status_code,
-            "message": message,
-        },
-        status_code=exception.status_code
-    )
-
-@app.exception_handler(RequestValidationError)
-def validation_exception_handler(request: Request, exception: RequestValidationError):
-    if request.url.path.startswith("/api"):
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-            content={"detail": exception.errors()},
-        )
-    
-    return templates.TemplateResponse(
-        request,
-        "error.html",
-        {
-            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
-            "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
-            "message" : "Invalid request. Please check your input and try again."
-        },
-        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-    )
---
+++ router/user_router.py
@@ -0,0 +1,65 @@
+from fastapi import APIRouter, Depends, HTTPException, status, Request
+from schemas.user_schema import UserCreate, UserResponse
+from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from models.users_model import User
+from models.posts_model import Post
+from database.database import get_db
+from schemas.post_schema import PostResponse
+
+
+router = APIRouter(prefix="/api", tags=["Users"])
+
+@router.post("/users", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(User).where(User.username == user.username))
+    existing_user = result.scalars().first()
+    if existing_user :
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists",
+
+        )
+    
+    result = db.execute(select(User).where(User.email == user.email))
+    existing_email = result.scalars().first()
+
+    if existing_email:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email Address already exists"
+        )
+    
+    new_user = User(
+        username = user.username,
+        email = user.email,
+
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+
+@router.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(User).where(User.id == user_id))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with {user_id} not found.")
+    
+    return user
+
+@router.get("/users/{user_id}/posts", response_model=list[PostResponse])
+def get_user_posts(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(User).where(User.id == user_id))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with {user_id} not found")
+    
+    result = db.execute(select(Post).where(Post.user_id==user_id))
+    posts = result.scalars().first()
+    return posts
